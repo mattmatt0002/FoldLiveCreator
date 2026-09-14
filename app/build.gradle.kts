a7317c47a28bbd16a24b0e318cf89a9eff1cc629
mattmatt0002 <mattf.hobbs@gmail.com>
2026-09-14 11:49:36 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,9 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
-    }     compileOptions {
+    }
+
+    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
